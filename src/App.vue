73a4cf44064601d74b77d32f099a35275fe3bdce
Alexandr Netsev <netsev@yandex.ru>
2019-03-19 00:23:52 +0500

    Stretch pixi container.

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,16 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+html, body {
+  height: 100%;
+}
 #app {
+  width: 100%;
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
